f38fe29986601da0b9333279d8c526ab8059336f
Unknown <bradenmars02@gmail.com>
2018-05-08 09:56:02 -0500

    Analog Light Sensor Test
    Analog Light Sensor Lux Output Test

--- LaserHarp/src/main.cpp
+++ LaserHarp/src/main.cpp
@@ -1,38 +1,28 @@
-#include <Arduino.h>
-#include <AccelStepper.h>
-#define HALFSTEP 8
+ #include <Arduino.h>
 
-// Constants
-const int revSteps = 64;
-const int notes = 6;
+int sensorPin = A0;
+float rawRange = 1024; // 3.3v
+float logRange = 5.0; // 3.3v = 10^5 lux
 
-// Stepper
-int m1 = 8;
-int m2 = 9;
-int m3 = 10;
-int m4 = 11;
-AccelStepper mirStepper(AccelStepper::FULL4WIRE, m1, m2, m3, m4);
-
-void setup() {
-  mirStepper.setMaxSpeed(1000);
-  mirStepper.setAcceleration(100);
-  mirStepper.moveTo(4000);
+void setup()
+{
+  analogReference(EXTERNAL); //
+  Serial.begin(9600);
 }
 
-// void runMirror(int steps){
-//   for(int x=0; x<=notes; x++){
-//     mirStepper.step(steps);
-//     Serial.println('Completed Clockwise');
-//     delay(15);
-//   }
-//   delay(15);
-// }
-
+float RawToLux(int raw)
+{
+  float logLux = raw * logRange / rawRange;
+  return pow(10, logLux);
+}
 
 void loop()
 {
-    // If at the end of travel go to the other end
-    if (mirStepper.distanceToGo() == 0)
-      mirStepper.moveTo(-mirStepper.currentPosition());
-    mirStepper.run();
+  int rawValue = analogRead(sensorPin);
+
+  Serial.print("Raw = ");
+  Serial.print(rawValue);
+  Serial.print(" - Lux = ");
+  Serial.println(RawToLux(rawValue));
+  delay(1000);
 }
