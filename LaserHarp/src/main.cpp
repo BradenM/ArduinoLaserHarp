e64e47726dc45eea41c3405f69171afcd1d28be9
Unknown <bradenmars02@gmail.com>
2018-05-21 01:24:22 -0500

    Stepper and Beam Control
    Stepper:
    Back to normal stepper library with low revolution count to enable much faster speeds. This along with direct 9v plug in works great.
    
    Laser:
    Took laser apart and connected to to the Arduino via a PNP transistor switch, allow it to be rapidly turned on and off in sync with the stepper motor. Thus, removes the ghosting the beams previously had with the light constantly turned on.

--- LaserHarp/src/main.cpp
+++ LaserHarp/src/main.cpp
@@ -1,29 +1,31 @@
 #include <Arduino.h>
-#include <AccelStepper.h>
-#define HALFSTEP 8
-#define FULLSTEP 4
+#include <Stepper.h>
 
 // Light Sensor
 int lightSensor = A0;
 float rawRange = 1024; // 3.3v
-
-// Constants
-const int notes = 6;
-int inc = 8;
-int mod = 8;
-
-// Stepper
-int m1 = 8;
-int m2 = 9;
-int m3 = 10;
-int m4 = 11;
-AccelStepper mirStepper(FULLSTEP, m1, m3, m2, m4);
 float logRange = 5.0; // 3.3v = 10^5 lux
 
+
+// Harp Settings
+int direction = 1;
+const int notes = 4;
+int noteCount = 0;
+const int noteSpace = 16;
+const int beamDelay = 7;
+
+// Laser
+const int beamPin = 7;
+
+// Stepper
+const int revSteps = 16;
+Stepper harpStepper(revSteps, 8, 10, 9, 11);
+
+
 void setup() {
-  analogReference(EXTERNAL);
-  mirStepper.setMaxSpeed(6000);
-  mirStepper.setAcceleration(6000);
+  //analogReference(EXTERNAL);
+  pinMode(beamPin, OUTPUT);
+  harpStepper.setSpeed(2500);
   Serial.begin(9600);
 }
 
@@ -33,31 +35,19 @@ float RawToLux(int raw)
   return pow(10, logLux);
 }
 
-void enableBeams(){
-
-  for(int x = 0; x <= notes; x++){
-    mirStepper.runToNewPosition(inc);
-    inc = inc + mod;
-    mirStepper.stop();
-    int rawLight = analogRead(A0);
-    int light = RawToLux(rawLight);
-    if(light >= 150){
-      Serial.println("BREAK ON - ");
-      Serial.print(mod);
-    }
-    Serial.println(light);
-    delay(8);
-  }
-  if(mod > 0){
-    mod = -8;
-  }else{
-    mod = 8;
-  }
-}
-
 void loop() {
-    enableBeams();
+  harpStepper.step(noteSpace * direction);
+  noteCount += direction;
+  digitalWrite(beamPin, HIGH);
+  int rawLight = analogRead(A0);
+  int lightRead = RawToLux(rawLight);
+  //Serial.println(lightRead);
+  Serial.println(rawLight);
+  delay(beamDelay);
+  digitalWrite(beamPin, LOW);
 
-
+  if(noteCount == 0 || noteCount == notes){
+    direction *= (-1);
+  }
 
 }
